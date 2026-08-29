f92fea18afefbe57ca6e4370411c477d014d44ec
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2026-08-29 21:09:33 +0100

    refactor: extract router operations into service file within the package

--- router/router.go
+++ router/router.go
@@ -4,63 +4,9 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	db "local.learn.go/db"
 )
 
-func FindAll(context *gin.Context) {
-	movies, error := db.GetAllMovies()
-	if error != nil {
-		response := FromError(error)
-		context.JSON(response.Status, gin.H{"error": response.Error()})
-		return	
-	}
-
-	context.JSON(http.StatusOK, gin.H{"movies": movies})
-}
-
-func FindOne(context *gin.Context) {
-	id := context.Param("movieId")
-	movie, error := db.GetMovie(id)
-	if error != nil {
-		response := FromError(error)
-		context.JSON(response.Status, gin.H{"error": response.Error()})
-		return
-	}
-
-	context.JSON(http.StatusOK, gin.H{"movie": movie})
-}
-
-func Insert(context *gin.Context) {
-	var movie *db.Movie
-	contextError := context.Bind(&movie)
-	if contextError != nil {
-		response := FromError(contextError)
-		context.JSON(response.Status, gin.H{"error": response.Error()})
-		return
-	}
-
-	movie, error := db.CreateMovie(movie)
-	if error != nil {
-		response := FromError(error)
-		context.JSON(response.Status, gin.H{"error": response.Error()})
-		return
-	}
-
-	context.JSON(http.StatusOK, gin.H{"movie": movie})
-}
-
-func DeleteOne(context *gin.Context) {
-	id := context.Param("movieId")
-	
-	error := db.DeleteMovie(id)
-	if error != nil {
-		response := FromError(error)
-		context.JSON(response.Status, gin.H{"error": response.Error()})
-		return
-	}
-
-	context.JSON(http.StatusOK, gin.H{"message": "Removed successfully"})
-}
+// TO DO: implement midware to check auth headers
 
 func InitRouter() *gin.Engine {
 	router := gin.Default()
