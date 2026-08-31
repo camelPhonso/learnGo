24349930d3469626b3239e0f336bef40f4fcdbf9
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2026-08-31 19:25:03 +0100

    feat: serve html for new public page

--- router/router.go
+++ router/router.go
@@ -9,13 +9,23 @@ import (
 func InitRouter() *gin.Engine {
 	router := gin.Default()
 	router.Use(AuthMiddleware())
-
+	
 	router.GET("/health", func(context *gin.Context) { context.JSON(http.StatusOK, gin.H{"message": "Server is alive"})})
-
+	
 	router.GET("/movies", FindAll) 
 	router.GET("/movies/:movieId", FindOne)
 	router.POST("/movies", Insert)
 	router.DELETE("/movies/:movieId", DeleteOne)
-
+	
+	// html blob is served to public pages 
+	// authmiddleware is not applied to this group
+	router.LoadHTMLGlob("templates/*")
+	public := router.Group("/public")
+	{
+		public.GET("/", func(context *gin.Context) {
+			context.HTML(http.StatusOK, "index.html", gin.H{"title": "Uploading a file!"})
+		})
+	}
+	
 	return router
 }
