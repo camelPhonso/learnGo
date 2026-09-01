ad0f78d7227c58e3b1b0d4d29fba7d54b1ea1c2d
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2026-09-01 21:51:37 +0100

    test: create test unit for new helper function

---
+++ db/read_file_test.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"testing"
+)
+
+func TestRead(t *testing.T) {
+	tests := []struct {
+		name string
+		filePath string
+		want bool
+	}{
+		{"Valid scenario", "../test_movie_list.txt", false},
+		{"Invalid scenario", "../fake_file_path.txt", true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := Read(tt.filePath)
+
+			if (err == nil) != tt.want {
+				t.Errorf("Unexpected error: %v", err)
+			}
+		})
+	}
+}
